d63f86af5e38a9893a8e24afa3304ca94b7ac4b8
CosecSecCot <cosecseccot581@gmail.com>
2024-11-12 23:59:27 +0530

    fix: some keys wouldn't register when certain keys were being held down

--- src/EntityComponentSystem/Components/KeyboardInput.hpp
+++ src/EntityComponentSystem/Components/KeyboardInput.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Game.hpp"
-#pragma once
+#include <unordered_map>
 
 #include "EntityComponentSystem/Component.hpp"
 #include "EntityComponentSystem/Components/TransformComponent.hpp"
@@ -13,24 +13,51 @@ public:
     explicit KeyboardInput(TransformComponent &transform) : transform(&transform) {}
 
     void update() override {
-        transform->velocity.x = 0;
-        transform->velocity.y = 0;
+        switch (Game::event.type) {
+        case SDL_KEYDOWN:
+            keyboardState[Game::event.key.keysym.sym] = true;
+            break;
+        case SDL_KEYUP:
+            keyboardState[Game::event.key.keysym.sym] = false;
+            break;
+        default:
+            break;
+        }
 
-        if (keystates[SDL_SCANCODE_UP] || keystates[SDL_SCANCODE_W]) {
-            transform->velocity.y = -1;
+        // For each frame calculate velocity
+        // inidividually
+        transform->velocity = {0, 0};
+
+        if (keyboardState[SDLK_UP] || keyboardState[SDLK_w]) {
+            if (transform->velocity.y == 1)
+                transform->velocity.y = 0;
+            else
+                transform->velocity.y = -1;
         }
-        if (keystates[SDL_SCANCODE_LEFT] || keystates[SDL_SCANCODE_A]) {
-            transform->velocity.x = -1;
+
+        if (keyboardState[SDLK_LEFT] || keyboardState[SDLK_a]) {
+            if (transform->velocity.x == 1)
+                transform->velocity.x = 0;
+            else
+                transform->velocity.x = -1;
         }
-        if (keystates[SDL_SCANCODE_DOWN] || keystates[SDL_SCANCODE_S]) {
-            transform->velocity.y = 1;
+
+        if (keyboardState[SDLK_DOWN] || keyboardState[SDLK_s]) {
+            if (transform->velocity.y == -1)
+                transform->velocity.y = 0;
+            else
+                transform->velocity.y = 1;
         }
-        if (keystates[SDL_SCANCODE_RIGHT] || keystates[SDL_SCANCODE_D]) {
-            transform->velocity.x = 1;
+
+        if (keyboardState[SDLK_RIGHT] || keyboardState[SDLK_d]) {
+            if (transform->velocity.x == -1)
+                transform->velocity.x = 0;
+            else
+                transform->velocity.x = 1;
         }
     }
 
 private:
     TransformComponent *transform;
-    const Uint8 *keystates = SDL_GetKeyboardState(nullptr);
+    std::unordered_map<SDL_Keycode, bool> keyboardState;
 };
