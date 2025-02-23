5a7da3ca7097e5b2880758b66c0928be4f1d0861
CosecSecCot <cosecseccot581@gmail.com>
2025-02-23 22:06:26 +0530

    bonga

--- Sandbox/src/Application.cpp
+++ Sandbox/src/Application.cpp
@@ -1,29 +1,32 @@
-#include "imgui.h"
 #include <CosecEngine.h>
+#include <imgui.h>
 
 class ExampleLayer : public Cosec::Layer {
 public:
     ExampleLayer() : Cosec::Layer("Example") {};
 
     void OnUpdate() override {
-        if (Cosec::EngineInput::IsKeyPressed(COSEC_KEY_J)) {
-            LOG_TRACE("J key pressed!");
-        }
-        if (Cosec::EngineInput::IsMouseButtonPressed(COSEC_MOUSE_BUTTON_LEFT)) {
-            LOG_TRACE("LEFT MOUSE pressed!");
-        }
-        if (Cosec::EngineInput::IsMouseButtonPressed(COSEC_MOUSE_BUTTON_RIGHT)) {
-            LOG_TRACE("RIGHT MOUSE pressed!");
-        }
+        // if (Cosec::EngineInput::IsKeyPressed(COSEC_KEY_J)) {
+        //     LOG_TRACE("J key pressed!");
+        // }
+        // if (Cosec::EngineInput::IsMouseButtonPressed(COSEC_MOUSE_BUTTON_LEFT)) {
+        //     LOG_TRACE("LEFT MOUSE pressed!");
+        // }
     }
 
     void OnImGuiRender() override {
-        ImGui::Begin("Test");
-        ImGui::Text("Hello World!");
+        ImGui::Begin("Bonga");
+        ImGui::Checkbox("Are you Bonga ?", &m_IsBonga);
+        if (m_IsBonga) {
+            ImGui::Text("Hello Bonga!!");
+        }
         ImGui::End();
     }
 
     void OnEvent(Cosec::Event &event) override {}
+
+private:
+    bool m_IsBonga = false;
 };
 
 class Sandbox : public Cosec::Application {
